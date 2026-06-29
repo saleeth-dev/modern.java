e75f82a10d17565cfec1e13153bec74276f6774a
S H Mahammed Saleeth <selith514@gmail.com>
2026-06-29 22:09:54 +0530

    Add Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,20 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        String day = "Friday";
+        System.out.print("Enter the day of the week: ");
+        String day = sc.nextLine();
 
-        if(day.equals("Monday")) {
-            System.out.println("Today is Monday");
-        } else if(day.equals("Tuesday")) {
-            System.out.println("Today is Tuesday");
-        } else if(day.equals("Wednesday")) {
-            System.out.println("Today is Wednesday");
-        } else if(day.equals("Thursday")) {
-            System.out.println("Today is Thursday");
-        } else if(day.equals("Friday")) {
-            System.out.println("Today is Friday");
-        } else if(day.equals("Saturday")) {
-            System.out.println("Today is Saturday");
-        } else if(day.equals("Sunday")) {
-            System.out.println("Today is Weekend");
-        } else {
-            System.out.println("Invalid day");
+        switch(day){
+            case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" ->
+                System.out.println(day + " is a weekday.");
+            case "Saturday", "Sunday" ->
+                System.out.println(day + " is a weekend.");
+            default ->
+                System.out.println("Invalid day of the week.");
         }
+        sc.close();
+        
     }
 }
