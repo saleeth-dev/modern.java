f1cb5b10baec546c6fd507b86bd7ff350ac49aab
S H Mahammed Saleeth <selith514@gmail.com>
2026-06-30 21:14:20 +0530

    Add Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,33 @@
 import java.util.Scanner;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the day of the week: ");
-        String day = sc.nextLine();
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
 
-        int age = 0;
+        int guess;
+        int attempts = 0;
+        int min = 1;
+        int max = 100;
+        int randomNumber = random.nextInt(min, max + 1);
 
-        System.out.print("Enter your age: ");
-        age = sc.nextInt();
+        System.out.println("Number Guessing Game");
+        System.out.printf("Guess a number between %d-%d\n", min, max);
 
-        while (age < 0) {
-            System.out.println("Age cannot be negative. Please enter a valid age.");
-            System.out.print("Enter your age: ");
-            age = sc.nextInt();
-        }
+        do{
+            System.out.print("Enter a guess: ");
+            guess = scanner.nextInt();
+            attempts++;
 
-        System.out.println("Your age is: " + age);
-        sc.close();
-        
+            if(guess < randomNumber){
+                System.out.println("Too low! Try again.");
+            } else if(guess > randomNumber){
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.printf("Correct! You guessed the number in %d attempts.\n", attempts);
+            }
+        } while(guess != randomNumber);
+        scanner.close();
     }
 }
