d442f5c57da54838075d608141623cd98c8c6f26
S H Mahammed Saleeth <selith514@gmail.com>
2026-07-05 15:49:04 +0530

    Add dice rolling program

--- Main.java
+++ Main.java
@@ -1,24 +1,101 @@
+import java.util.Scanner;
+import java.util.Random;
 
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
 
-        String fullName = getFullName("John", "Doe");
-        System.out.println("FullName is " + fullName);
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+        int numofDice;
+        int total = 0;
+
+        System.out.print("Enter the # of dice to roll: ");
+        numofDice = sc.nextInt();
+
         
+        if(numofDice > 0) {
+
+            for(int i = 0; i < numofDice; i++) {
+                int roll = rand.nextInt(1, 7);
+                printDie(roll);
+                System.out.println("You rolled a " + roll);
+                total += roll;
+            }
+            System.out.println("Total: " + total);
+            
+        } else {
+            System.out.println("You must roll at least one die.");
+        }
+        sc.close();
     }
+    static void printDie(int roll) {
 
-        static void happyBirthday(String name, int age) {
-            System.out.println("Happy Birthday to you!");
-            System.out.printf("Happy Birthday %s Your age is now %d\n", name, age);
-            System.out.printf("Happy Birthday dear %s\n ", name);
-            System.out.println("Happy Birthday to you!\n");
-        }
-        static double square(double number) {
-            return number * number;
-        }
-        static String getFullName(String firstName, String lastName) {
-            return firstName + " " + lastName;
-        }
+        String dice1 = """
 
+                -----------
+                |         |
+                |    *    |
+                |         |
+                -----------
+                """;
+        String dice2 = """
+
+                -----------
+                |  *      |
+                |         |
+                |      *  |
+                -----------
+                """;    
+        String dice3 = """
+
+                -----------
+                |  *      |
+                |    *    |
+                |      *  |
+                -----------
+                """;
+        String dice4 = """
+                -----------
+                |  *   *  |
+                |         |
+                |  *   *  |
+                -----------
+                """;
+        String dice5 = """
+                -----------
+                |  *   *  |
+                |    *    |
+                |  *   *  |
+                -----------
+                """;
+        String dice6 = """
+                -----------
+                |  *   *  |
+                |  *   *  |
+                |  *   *  |
+                -----------
+                """;
+        switch (roll) {
+            case 1:
+                System.out.print(dice1);
+                break;
+            case 2:
+                System.out.print(dice2);
+                break;
+            case 3:
+                System.out.print(dice3);
+                break;
+            case 4:
+                System.out.print(dice4);
+                break;
+            case 5:
+                System.out.print(dice5);
+                break;
+            case 6:
+                System.out.print(dice6);
+                break;
+            default:
+                System.out.println("Invalid die roll.");
+        }
+    }
 }
-
