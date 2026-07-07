093a7485eb291d8727fe8065084253f292db779d
S H Mahammed Saleeth <selith514@gmail.com>
2026-07-07 09:31:13 +0530

    message

--- Super.java
+++ Super.java
@@ -1,22 +1,15 @@
 
 public class Super {
     public static void main(String[] args) {
-       
-    int[] numbers = {3, 5, 9, 2, 8, 4};
-    int target = 12;
-    boolean isFound = false;
 
-    for(int i = 0; i < numbers.length; i++) {
-        if(target == numbers[i]) {
-            System.out.println("Found the target number: " + i);
-            isFound = true;
-            break;
-        }
-    }
-
-    if(!isFound) {
-        System.out.println("Element is notfound in the array.");
-    }
+        System.out.println(average(1, 2, 3, 4));
     
 }
+static double average(double... numbers) {
+        double sum = 0;
+        for (double number : numbers) {
+            sum += number;
+        }
+        return sum / numbers.length;
+    }
 }
